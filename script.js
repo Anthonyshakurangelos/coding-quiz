761e7a518bed4d9b7cfebb199f5a3154b4a6660d
Anthonyshakurangelos <anthonyshakurangelos@gmail.com>
2022-11-20 17:57:12 -0700

    added a section to submit score and name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var viewHighScore = document.querySelector(".view-highscore");
 var result = document.querySelector(".result");
 var displayQuestion = document.querySelector("#question");
-var resetButton = document.querySelector(".reset-button");
+var saveButton = document.querySelector(".save-score-button");
 var timerElement = document.querySelector(".timer-count");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
@@ -13,6 +13,7 @@ var opt3 = document.querySelector("#opt3");
 var opt4 = document.querySelector("#opt4");
 var optionButton = document.querySelectorAll(".option-container")
 
+var gameOver;
 var score = 0;
 var index = 0;
 var winCount = 0;
@@ -21,6 +22,7 @@ var isWin = false;
 var timer;
 var rightAnswer = "";
 var timerCount;
+let finalTime = 0;
 var question = [
     {
         question: "what does javascript do?",
@@ -39,7 +41,7 @@ var question = [
      choices: ["()", "{}", "[]", "//"],
     rightAnswer: "()"}
 ]
-//repeat this process and clean up, look up key value pairs.
+// look up key value pairs.
  function init() {
     getWins();
     getLosses();
@@ -73,19 +75,20 @@ function startTimer() {
 
     timer = setInterval(function() {
         timerCount--;
-        timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-        if(Win && timerCount > 0) {
+        timerElement.textContent = timerCount + " seconds remaining";
+    if (timerCount === 0) {
+       // if(Win && timerCount > 0) {
             clearInterval(timer);
-            winGame();
-        }
+            timerElement.innerHTML = ""
+           // winGame();
+       // }
     }
     console.log(timer)
-    if (timerCount === 0) {
+   // if (timerCount === 0) {
 
-        clearInterval(timer);
-        loseGame();
-    }
+    //    clearInterval(timer);
+     //   loseGame();
+  //  }
   }, 1000);
 }
  function loadQuestion() {
@@ -95,41 +98,70 @@ function startTimer() {
   opt2.innerText = question[index].choices[1]
   opt3.innerText = question[index].choices[2]
   opt4.innerText = question[index].choices[3]
+  
+   
+ }
+ function initialsPage() {
+  document.querySelector(".panel").innerHTML = ""
+  let finalScorediv = document.createElement("div")
+  finalScorediv.innerHTML = `your final score is ${finalTime}`
+  let initialsdiv = document.createElement("div")
+  initialsdiv.style.display = "flex"
+  let message_para = document.createElement("p")
+  message_para.innerHTML = "enter your name";
+  let inputEl = document.createElement("input")
+  initialsdiv.append(message_para, inputEl)
+  let submitbtn = document.createElement("button")
+  submitbtn.innerHTML = "submit score"
+  document.querySelector(".panel").append(finalScorediv, initialsdiv,submitbtn)
+
  }
  function nextQuestion() {
     console.log(index)
+  if (index < 3){
+
     index++
     loadQuestion()
-
-    
+  } else {
+    finalTime = timerCount
+    initialsPage()
+  } 
  }
+    
+ 
  function optionChoice(e) {
    var choseAnswer = e.target.innerText
    console.log(choseAnswer)
    if(choseAnswer==question[index].rightAnswer) {
     winCount++
     win.textContent = winCount;
-
+     
    }  
-    
+    else if(choseAnswer!=question[index].rightAnswer) {
+      timerCount--;
+      loseCount++
+      lose.textContent = loseCount;
+    }
      
   }
+    // need to save results
+    function saveResults() {
+     
+    }
     
-    //do else if statements
-
-   
+    function gameOver() {
+      if (timerCount==0 && question[3].choices ) {
+         
+      }
+    }
+   // need to display results and be able to save with initials 
+ // once all questions are answered or timer is 0 then 
  
- 
-  // function logScore()
-// if (question[index]===rightAnswer) {
-  //  winCount++
-//}
 start.addEventListener("click", startGame)
 next.addEventListener("click", nextQuestion)
-//optionButton.addEventListener("click", )
-//do a else if statement, 
 opt1.addEventListener("click", optionChoice)
 opt2.addEventListener("click", optionChoice)
 opt3.addEventListener("click", optionChoice)
 opt4.addEventListener("click", optionChoice)
- //add rest of opt
+saveButton.addEventListener("click", saveResults)
+// fix timer and log score in view high score and log high score with 
